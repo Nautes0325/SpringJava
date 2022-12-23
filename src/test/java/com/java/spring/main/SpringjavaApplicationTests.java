5b46cf595105a980e7fd6798f181b5fc92f94cc0
unknown <nautes1109@gmail.com>
2022-12-23 22:12:36 +0900

    first commit

---
+++ src/test/java/com/java/spring/main/SpringjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.spring.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
